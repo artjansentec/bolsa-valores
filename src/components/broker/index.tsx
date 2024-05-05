634f59487e232c7ef72432101d2fcd7a7edc439f
Arthur Jansen <artjansentec@gmail.com>
2024-05-05 19:01:23 -0300

    novos dados

--- src/components/broker/index.tsx
+++ src/components/broker/index.tsx
@@ -6,25 +6,22 @@ import Grid from '@mui/material/Grid';
 import { Input, FormControl, Typography, MenuItem, Select, InputLabel } from '@mui/material';
 import imgLogo from '../../img/XPLogo.jpg'
 import { useState } from 'react';
-import { useApi } from '../../apis/axiosInstance.ts';
-
+import axios from 'axios';
 
 const acoesNames = [
   "ABEV3", "PETR4", "VALE5", "ITUB4", "BBDC4", "BBAS3", "CIEL3", "PETR3", "HYPE3", "VALE3", "BBSE3", "CTIP3", "GGBR4", "FIBR3", "RADL3"
 ];
 
-
 function Broker() {
-  
-  const [Acao, setAcao ]            = useState("");
-  const [Tipo, setTipo ]            = useState("");
-  const [Valor, setValor]           = useState("");
+  const [Acao, setAcao] = useState("");
+  const [Tipo, setTipo] = useState("");
+  const [Valor, setValor] = useState("");
   const [Quantidade, setQuantidade] = useState("");
 
   function handleTipo(event) {
     setTipo(event.target.value);
   }
-  
+
   function handleAcao(event) {
     setAcao(event.target.value);
   }
@@ -38,38 +35,30 @@ function Broker() {
   }
 
   function handleEnvia() {
+    if (!Tipo || !Acao || !Valor || !Quantidade) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
 
-    let data = JSON.stringify({
-      "codigoAcao": Acao,
-      "quantidade": Quantidade,
-      "valor": Valor,
-      "corretora": "XPTO"
-    });
-
-
-    let config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://rabbitmq-bolsadevalores.onrender.com/'+Tipo,
-      headers: { 
-        'Content-Type': 'application/json'
-      },
-      data : data
+    const data = {
+      codigoAcao: Acao,
+      quantidade: Quantidade,
+      valor: Valor,
+      corretora: "XPTO"
     };
-    
-    useApi.request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
 
+    axios.post('https://rabbitmq-bolsadevalores.onrender.com/' + Tipo, data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('Erro ao enviar mensagem para a fila RabbitMQ:', error);
+      });
   }
 
   return (
     <Grid container component="main" sx={{ height: '100vh' }}>
-      <img src={imgLogo}  alt="" />
+      <img src={imgLogo} alt="" />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <Box
           sx={{
@@ -86,7 +75,6 @@ function Broker() {
           </Typography>
 
           <FormControl sx={{ m: 1, minWidth: 120 }}>
-
             <InputLabel id="tipo-aporte-label">Tipo</InputLabel>
             <Select
               labelId="tipo-aporte-label"
@@ -95,22 +83,14 @@ function Broker() {
               value={Tipo}
               onChange={handleTipo}
             >
-              <MenuItem
-                key={'comprar'}
-                value={'comprar'}
-              >
+              <MenuItem key={'comprar'} value={'comprar'}>
                 {'Comprar'}
               </MenuItem>
 
-              <MenuItem
-                key={'vender'}
-                value={'vender'}
-              >
+              <MenuItem key={'vender'} value={'vender'}>
                 {'Vender'}
               </MenuItem>
-          
             </Select>
-            
           </FormControl>
           <br /> <br />
 
@@ -127,16 +107,12 @@ function Broker() {
               value={Acao}
               onChange={handleAcao}
             >
-            {acoesNames.map((name) => (
-                <MenuItem
-                key={name}
-                value={name}
-                >
-                {name}
+              {acoesNames.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name}
                 </MenuItem>
-            ))}
+              ))}
             </Select>
-            
           </FormControl>
           <br /> <br />
 
@@ -146,11 +122,7 @@ function Broker() {
 
           <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
             <InputLabel htmlFor="valor-acao">Valor</InputLabel>
-            <Input
-              id="valor-acao"
-              type={'number'}
-              onChange={handleValor}
-            />
+            <Input id="valor-acao" type={'number'} onChange={handleValor} />
           </FormControl>
           <br /> <br />
 
@@ -160,21 +132,17 @@ function Broker() {
 
           <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
             <InputLabel htmlFor="Quantidade-acao">Quantidade</InputLabel>
-            <Input
-              id="Quantidade-acao"
-              type={'number'}
-              onChange={handleQuantidade}
-            />
+            <Input id="Quantidade-acao" type={'number'} onChange={handleQuantidade} />
           </FormControl>
-            
-            <Button
-              type="submit"
-              variant="contained"
-              onClick={handleEnvia}
-              sx={{ mt: 3, mb: 2, width: 2 }}
-            >
-              Enviar
-            </Button>
+
+          <Button
+            type="submit"
+            variant="contained"
+            onClick={handleEnvia}
+            sx={{ mt: 3, mb: 2, width: 2 }}
+          >
+            Enviar
+          </Button>
         </Box>
       </Grid>
     </Grid>
